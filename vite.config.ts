ac069c875565fff8198f3e88fd91d3479680edab
Kakarotto <register_2019@126.com>
2023-10-14 17:31:29 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/my-blog',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
